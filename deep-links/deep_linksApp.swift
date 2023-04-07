5f6c903b1149fc2e1cbb8e805071cda5ee4f3866
hoshi005 <hoshi005@me.com>
2023-04-08 08:18:44 +0900

    Initial Commit

---
+++ deep-links/deep_linksApp.swift
@@ -0,0 +1,17 @@
+//
+//  deep_linksApp.swift
+//  deep-links
+//
+//  Created by Susumu Hoshikawa on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct deep_linksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
